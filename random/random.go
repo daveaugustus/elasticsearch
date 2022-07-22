3ca84b1e1e26c0b2a4cdb679be917a9bcc4bda8a
Dave Augustus <davetweetlive@gmail.com>
2022-07-22 16:54:46 +0530

    Done changes
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- random/random.go
+++ random/random.go
@@ -11,30 +11,30 @@ func init() {
 	rand.Seed(time.Now().UnixNano())
 }
 
-// NewName() return a random full name
+// NewName() return a random full name first name will be 3 to 6 chars long
+// last name will be 4 to 9 chars log
 func NewName() string {
 	var letters = []rune(constants.AlphabetsLower)
+	lenFName := RandomNoNToN(3, 6)
+	lenLName := RandomNoNToN(4, 9)
 
-	firstName := make([]rune, 5)
-	lastName := make([]rune, 5)
+	firstName := make([]rune, lenFName)
+	lastName := make([]rune, lenLName)
+
 	for i := range firstName {
 		firstName[i] = letters[rand.Intn(len(letters))]
+	}
+
+	for i := range lastName {
 		lastName[i] = letters[rand.Intn(len(letters))]
 	}
 	return strings.Title(string(firstName)) + " " + strings.Title(string(lastName))
 }
 
-func NewAge() int {
-	maxAge := 90
-	minAge := 13
-	age := rand.Intn(maxAge-minAge) + minAge
-	return age
-}
-
 func NewEmail(name string) string {
 	name = strings.ToLower(name)
 	username := strings.Replace(name, " ", "_", 1)
-	emailDomains := EmailProviders()
+	emailDomains := EmailDomains()
 
 	domainIndex := rand.Intn(len(emailDomains))
 
@@ -45,7 +45,18 @@ func NewTime() {
 
 }
 
-func EmailProviders() []string {
+// EmailDomains returns a slice of email domains
+func EmailDomains() []string {
 	return []string{"@gmail.com", "@outlook.com", "@yahoo.com", "@hotmail.com",
-		"@rediffmail.com", "@protonmail.com", "@zoho.com", "@icloud.com", "@mail.com"}
+		"@protonmail.com", "@zoho.com", "@icloud.com", "@mail.com"}
+}
+
+// RandomNoZeroToN returns a random no from 0 to n
+func RandomNoZeroToN(n int) int {
+	return rand.Intn(n)
+}
+
+// RandomNoZeroToN returns a random no from min to max
+func RandomNoNToN(min, max int) int {
+	return rand.Intn(max-min) + min
 }
--- random/random_test.go
+++ random/random_test.go
@@ -7,26 +7,36 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewAge(t *testing.T) {
+func TestRandomNoNToN(t *testing.T) {
 	tests := []struct {
 		name string
+		max  int
+		min  int
 	}{
 		{
 			name: "Test 1",
+			max:  90,
+			min:  13,
 		},
 		{
 			name: "Test 2",
+			max:  90,
+			min:  13,
 		},
 		{
 			name: "Test 3",
+			max:  90,
+			min:  13,
 		},
 		{
 			name: "Test 4",
+			max:  90,
+			min:  13,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := NewAge()
+			got := RandomNoNToN(tt.min, tt.max)
 			log.Println("Got: ", got)
 			require.NotNil(t, got)
 			require.GreaterOrEqual(t, got, 13)
