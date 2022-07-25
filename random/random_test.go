508800a3f1b09deb996ab6379cf60689d072956d
Dave Augustus <davetweetlive@gmail.com>
2022-07-25 23:33:19 +0530

    Test cases updated
    Signed-off-by: Dave Augustus <davetweetlive@gmail.com>

--- random/random_test.go
+++ random/random_test.go
@@ -2,6 +2,7 @@ package random
 
 import (
 	"log"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -48,5 +49,47 @@ func TestRandomNoNToN(t *testing.T) {
 func TestRandomNoZeroToN(t *testing.T) {
 	number := RandomNoZeroToN(2)
 	log.Println(number)
-	require.NotZero(t, number)
+	require.GreaterOrEqual(t, number, 0)
+	require.LessOrEqual(t, number, 2)
+}
+
+func TestNewName(t *testing.T) {
+	fullName := NewName()
+	require.NotEmpty(t, fullName)
+	require.LessOrEqual(t, len(fullName), 15)
+	require.GreaterOrEqual(t, len(fullName), 7)
+
+	fullNameSlice := strings.Split(fullName, " ")
+	require.Equal(t, 2, len(fullNameSlice))
+	require.GreaterOrEqual(t, len(fullNameSlice[0]), 3)
+	require.LessOrEqual(t, len(fullNameSlice[0]), 6)
+	require.GreaterOrEqual(t, len(fullNameSlice[1]), 4)
+	require.LessOrEqual(t, len(fullNameSlice[1]), 9)
+}
+
+func TestEmailDomains(t *testing.T) {
+	domains := EmailDomains()
+	require.NotNil(t, domains)
+	require.NotEmpty(t, domains)
+	require.Equal(t, 8, len(domains))
+}
+
+func TestNewEmail(t *testing.T) {
+	fullName := NewName()
+	require.NotEmpty(t, fullName)
+	require.LessOrEqual(t, len(fullName), 15)
+	require.GreaterOrEqual(t, len(fullName), 7)
+
+	fullNameSlice := strings.Split(fullName, " ")
+	require.Equal(t, 2, len(fullNameSlice))
+	require.GreaterOrEqual(t, len(fullNameSlice[0]), 3)
+	require.LessOrEqual(t, len(fullNameSlice[0]), 6)
+	require.GreaterOrEqual(t, len(fullNameSlice[1]), 4)
+
+	// GetEmail passing the full name of the users
+	email := NewEmail(fullName)
+	require.NotEmpty(t, email)
+	require.Contains(t, email, "@")
+	require.Contains(t, email, ".com")
+	require.Contains(t, email, "_")
 }
